6a3cb14c04cba33b4cfef7e72d526f7438fd5092
Akshay Mayekar <akshaymayekar@Akshays-MacBook-Pro.local>
2020-01-14 18:45:50 +0530

    Initial setup

---
+++ src/components/layout.js
@@ -0,0 +1,51 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title,
+          description
+        }
+      }
+    }
+  `)
+
+  return (
+    <div>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer
+          style={{
+            position: `fixed`,
+            bottom: 0,
+            width: `100%`,
+            height: `30px`,
+          }}>
+          © {new Date().getFullYear()}, {` `}
+          <a href="https://www.equalexperts.com">Equal Experts</a>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h2>Hi people</h2>
+    <p>Welcome to Equal Experts Germany</p>
+    <p>Let's build something great.</p>
+  </Layout>
+)
+
+export default IndexPage
