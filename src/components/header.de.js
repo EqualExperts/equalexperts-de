e8831f872b2fbd6d7a0aa298fcfd201ab6e970ab
deepakverma05 <dverma@equalexperts.com>
2020-01-27 13:26:59 +0530

    modified window condition

--- src/components/header.de.js
+++ src/components/header.de.js
@@ -27,7 +27,9 @@ const HeaderDe = () => {
         }
       `);
 
-    const url = window? window.location.pathname:'';
+
+    const url = typeof window !== 'undefined' ? window.location.pathname : '';
+
     const { langs, defaultLangKey } = dataQuery.site.siteMetadata.languages;
     const langKey = getCurrentLangKey(langs, defaultLangKey, url);
     const homeLink = `/${langKey}/`;
--- src/components/header.en-US.js
+++ src/components/header.en-US.js
@@ -26,7 +26,7 @@ const HeaderDe = () => {
           }
         }
       `);
-    const url = window? window.location.pathname:'';
+    const url = typeof window !== 'undefined' ? window.location.pathname : '';
     const { langs, defaultLangKey } = dataQuery.site.siteMetadata.languages;
     const langKey = getCurrentLangKey(langs, defaultLangKey, url);
     const homeLink = `/${langKey}/`;
