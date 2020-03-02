0ac3e50cbc3cb8a0868e6d30d72f1dc00c3ee2eb
Simon Shepard <simon.shepard@gmail.com>
2020-03-02 12:06:42 +0100

    added slavery policy

---
+++ src/pages/modernSlaveryPolicy.de.js
@@ -0,0 +1,41 @@
+import React from "react"
+import LayoutDe from "../components/layout.de-DE"
+import SEO from "../components/seo"
+import "../styles/index.scss"
+import {graphql} from "gatsby"
+import LegalLayout from "../components/legal_layout";
+
+
+class ImprintPage extends React.Component {
+  render() {
+    
+    const { data } = this.props;
+    const pageData = data.allContentfulModuleTitleAndText.edges[0].node;
+    return (
+      <LayoutDe location={this.props.location}>
+        <SEO title={pageData.title} />
+        <LegalLayout
+          title={pageData.title}
+          data={pageData.childContentfulModuleTitleAndTextTextRichTextNode.json.content}
+        />
+      </LayoutDe>
+    )
+  }
+}
+
+export default ImprintPage
+
+export const pageQuery = graphql`
+query {
+  allContentfulModuleTitleAndText(filter: {node_locale: {eq: "de"}, title : {eq : "Modern Slavery Policy"}}) {
+    edges {
+      node {
+        title
+        childContentfulModuleTitleAndTextTextRichTextNode {
+          json
+        }
+      }
+    }
+  }
+}
+`
---
+++ src/pages/modernSlaveryPolicy.en-US.js
@@ -0,0 +1,41 @@
+import React from "react"
+import LayoutUS from "../components/layout.en-US"
+import SEO from "../components/seo"
+import "../styles/index.scss"
+import {graphql} from "gatsby"
+import LegalLayout from "../components/legal_layout";
+
+
+class ImprintPage extends React.Component {
+  render() {
+    
+    const { data } = this.props;
+    const pageData = data.allContentfulModuleTitleAndText.edges[0].node;
+    return (
+      <LayoutUS location={this.props.location}>
+        <SEO title={pageData.title} />
+        <LegalLayout
+          title={pageData.title}
+          data={pageData.childContentfulModuleTitleAndTextTextRichTextNode.json.content}
+        />
+      </LayoutUS>
+    )
+  }
+}
+
+export default ImprintPage
+
+export const pageQuery = graphql`
+query {
+  allContentfulModuleTitleAndText(filter: {node_locale: {eq: "en-US"}, title : {eq : "Modern Slavery Policy"}}) {
+    edges {
+      node {
+        title
+        childContentfulModuleTitleAndTextTextRichTextNode {
+          json
+        }
+      }
+    }
+  }
+}
+`
