4e51a63a3f1fe13371b6f94ab6aa2026b6a67cf7
Simon Shepard <simon.shepard@gmail.com>
2020-04-28 14:47:09 +0200

    added in missing defensive check for missing image in cms

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -40,7 +40,7 @@ class CTAModule extends React.Component {
                 )
               }
               let image;
-              if(item.ctaImage.file) {
+              if(item.ctaImage && item.ctaImage.file) {
                 image = (<img src={item.ctaImage.file.url} className="services__ctaImage" />);
               }
               return (
